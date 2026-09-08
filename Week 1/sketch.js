1d0e6fd14c923311e15824fcb00fa2e393f7dcb5
Vico-T <vicotukkera@outlook.com>
2026-09-08 15:56:37 +0200

    opdracht week 1

--- Week 1/sketch.js
+++ Week 1/sketch.js
@@ -1,8 +1,104 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(900, 900);
 }
 
 function draw() {
   background(220);
-  
+  fill('black');
+  //opdracht 1
+  text('1 Vico Tukker',10,10)
+  text('2',10,50)
+  text('3',10,120);
+  text('4',10,210);
+  text('5',130,10);
+  text('6',130,170);
+  text('7 Mario',220,20);
+  text('8 Yi',500,20);
+  //opdracht 2
+  fill('green');
+  rect(20,40,100,50);
+  strokeWeight(0);
+  fill('orange');
+  rect(20,60,100,10);
+  rect(65,40,10,50);
+  fill('black');
+  //opdracht 3
+  fill('white');
+  strokeWeight(2);
+  square(20,110,71);
+  fill('black');
+  square(20,110,24);
+  square(46,135,20);
+  square(68,110,23);
+  square(20,157,24);
+  square(68,157,23);
+  //opdracht 4
+  noFill()
+  triangle(20,250,50,210,80,250);
+  square(20,250,60);
+  //opdracht 5
+  fill('gray');
+  rect(150,10,30,80);
+  rect(158,90,15,50);
+  fill('red');
+  circle(165,25,20);
+  fill('orange')
+  circle(165,50,20);
+  fill('green');
+  circle(165,75,20);
+  //opdracht 6
+  strokeWeight(2);
+  fill('white');
+  square(150,170,50,10);
+  fill('black');
+  //rechts
+  circle(165,180,10);
+  circle(165,195,10);
+  circle(165,210,10);
+  //links
+  circle(185,180,10);
+  circle(185,195,10);
+  circle(185,210,10);
+  //opdracht 7
+  strokeWeight(0);
+  fill(355,204,153);
+  rect(350,50,60,40);
+  rect(340,60,10,20);
+  rect(350,90,90,10);
+  rect(410,60,40,20);
+  rect(450,70,10,10);
+  rect(320,130,30,30);
+  rect(410,130,30,30);
+  fill('brown');
+  rect(340,50,30,10);
+  rect(350,60,10,20);
+  rect(350,70,20,10);
+  rect(330,60,10,30); 
+  rect(340,80,10,10);
+  rect(330,170,30,20);
+  rect(320,180,10,10);
+  rect(400,170,30,20);
+  rect(430,180,10,10);
+  fill('black');
+  rect(400,80,50,10);
+  rect(400,50,10,20);
+  rect(410,70,10,10);
+    fill('red');
+  rect(350,30,60,20);
+  rect(340,40,110,10);
+  rect(340,100,70,40);
+  rect(330,110,100,20);
+  rect(320,120,120,10);
+  rect(410,130,10,10);
+  fill('blue');
+  rect(350,130,60,30);
+  rect(360,120,40,10);
+  rect(360,100,10,20);
+  rect(390,110,10,10);
+  rect(340,150,30,20);
+  rect(390,150,30,20);
+  fill('yellow');
+  rect(360,130,10,10);
+  rect(390,130,10,10);
+  //opdracht 8
 }
