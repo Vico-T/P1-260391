9d4ad76b228e333ddd0847cb50cfd308c148f080
Vico-T <vicotukkera@outlook.com>
2026-09-08 13:39:23 +0200

    mondriaan opdracht

--- Oefenopdrachten/Opdracht 1/sketch.js
+++ Oefenopdrachten/Opdracht 1/sketch.js
@@ -1,7 +1,28 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 400);
 }
 
 function draw() {
-  background(220);
+  background(255);
+  strokeWeight(5);
+  fill('nofill');
+  rect(5,5,590,390);
+  rect(95,50,50,150);
+  rect(425,195,90,85);
+    fill('yellow');
+  rect(95,200,300,80);
+  rect(20,30,300,20);
+  fill('red');
+  rect(395,5,30,275);
+  rect(5,5,100,50);
+  fill('blue');
+  rect(5,55,20,300);
+  rect(500,5,95,90);
+  rect(185,280,180,115);
+  fill('nofill');
+  rect(5,280,180,115);
+  fill('black');
+  rect(495,195,100,200);
+  fill('nofill');
+  rect(300,5,95,150);
 }
