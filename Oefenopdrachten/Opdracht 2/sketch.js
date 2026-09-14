1acd057c0037c326682639cd2969865ad801d464
Vico-T <vicotukkera@outlook.com>
2026-09-14 11:40:51 +0200

    weekopdracht 2

--- Oefenopdrachten/Opdracht 2/sketch.js
+++ Oefenopdrachten/Opdracht 2/sketch.js
@@ -1,7 +1,34 @@
+let variabellen = 100;
+let greeting = 'Hello World!';
+let a = 20;
+let b = 10;
+let y = 80;
+let x = 20;
+let optellen = a + b;
+let aftrekken = a - b;
+let delen = a / b;
+let keer = a * b;
+
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
   background(220);
+
+  //opdracht 1
+  text(variabellen,x,20);
+
+  //opdracht 2
+  text(greeting,x,60);
+
+  //opdracht 3 + 4
+  text('optellen: ' + optellen,x,y);
+  noLoop(y=y+ 20);
+  text('aftrekken: ' + aftrekken,x,y);
+  noLoop(y=y+ 20);
+  text('delen: ' + delen,x,y);
+  noLoop(y=y+ 20);
+  text('keer: ' + keer,x,y);
+  noLoop(y=y+ 20);
 }
--- Week 2/sketch.js
+++ Week 2/sketch.js
@@ -1,7 +1,22 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 600);
 }
-
+let daglicht = 'lightblue'
+let gebouw = 'lightgrey'
 function draw() {
-  background(220);
+  background(daglicht);
+  fill(gebouw);
+  //van links naar rechts worden de gebouwen geteld
+  //gebouw 3
+  rect(70,150,150,450);
+  //gebouw 5
+  rect(280,120,100,480);
+  //gebouw 4
+  rect(200,400,150,200);
+  //gebouw 1
+  rect(0,100,50,500);
+  //gebouw 2
+  rect(40,250,100,350);
+
+
 }
